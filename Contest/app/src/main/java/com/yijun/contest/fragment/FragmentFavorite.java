950e9b7f68f27add7ec6dffe9c20b8db88f23eb6
sen9040 <62924580+sen9040@users.noreply.github.com>
2020-08-17 22:38:49 +0900

    ButtonNavigationView 검색만 적용

---
+++ Contest/app/src/main/java/com/yijun/contest/fragment/FragmentFavorite.java
@@ -0,0 +1,18 @@
+package com.yijun.contest.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.yijun.contest.R;
+
+public class FragmentFavorite extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+}
---
+++ Contest/app/src/main/java/com/yijun/contest/fragment/FragmentSearch.java
@@ -0,0 +1,142 @@
+package com.yijun.contest.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.yijun.contest.R;
+import com.yijun.contest.list.ListActivity;
+import com.yijun.contest.list.adapter.RecyclerViewAdapter;
+import com.yijun.contest.model.SportInfo;
+import com.yijun.contest.search.SearchActivity;
+import com.yijun.contest.utils.Utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class FragmentSearch extends Fragment {
+
+    Context context;
+    Button btnSearch;
+
+    public FragmentSearch(){
+
+    }
+    public FragmentSearch(Context context){
+        this.context = context;
+    }
+    String testUrl = "http://openapi.seoul.go.kr:8088/765867555473656e353874786d6572/json/ListPublicReservationSport/1/5/%ED%85%8C%EB%8B%88%EC%8A%A4%EC%9E%A5";
+
+    int list_total_count;
+    RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
+    ArrayList<SportInfo> sportInfoArrayList = new ArrayList<>();
+
+    EditText editSearch;
+
+
+    RequestQueue requestQueue;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        editSearch = view.findViewById(R.id.editSearch);
+        btnSearch = view.findViewById(R.id.btnSearch);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestQueue = Volley.newRequestQueue(context);
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, testUrl, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("AAA","search response : "+response);
+                        try {
+                            JSONObject sport = response.getJSONObject("ListPublicReservationSport");
+                            list_total_count = sport.getInt("list_total_count");
+                            JSONArray row = sport.getJSONArray("row");
+                            for(int i = 0; i < row.length(); i++){
+
+                                JSONObject object = row.getJSONObject(i);
+                                String svcId = object.getString("SVCID");
+                                String maxClassNm= object.getString("MAXCLASSNM");
+                                String minClassNm= object.getString("MINCLASSNM");
+                                String svcStaTnm= object.getString("SVCSTATNM");
+                                String svcNm= object.getString("SVCNM");
+                                String paYaTnm= object.getString("PAYATNM");
+                                String placeNm= object.getString("PLACENM");
+                                String useTgtInfo= object.getString("USETGTINFO");
+                                String svcUrl= object.getString("SVCURL");
+                                String x= object.getString("X");
+                                String y= object.getString("Y");
+                                String svcOpnBgnDt= object.getString("SVCOPNBGNDT");
+                                String svcOpnEndDt= object.getString("SVCOPNENDDT");
+                                String rcptEndDt= object.getString("RCPTBGNDT");
+                                String rcptBgnDt= object.getString("RCPTENDDT");
+                                String areaNm= object.getString("AREANM");
+                                String imgUrl= object.getString("IMGURL");
+                                String dtlCont= object.getString("DTLCONT");
+                                String telNo= object.getString("TELNO");
+                                String v_min= object.getString("V_MIN");
+                                String v_max= object.getString("V_MAX");
+                                String revStdDayNm= object.getString("REVSTDDAYNM");
+                                String revStdDay= object.getString("REVSTDDAY");
+                                Log.i("AAA","search for : "+svcId);
+
+                                SportInfo sportInfo = new SportInfo(svcId,maxClassNm,minClassNm,svcStaTnm,svcNm,paYaTnm,
+                                        placeNm,useTgtInfo,svcUrl,x,y,svcOpnBgnDt,svcOpnEndDt,rcptBgnDt,rcptEndDt,areaNm,imgUrl,
+                                        dtlCont,telNo,v_min,v_max,revStdDayNm,revStdDay);
+                                sportInfoArrayList.add(sportInfo);
+                            }
+                            adapter = new RecyclerViewAdapter(context,sportInfoArrayList);
+                            recyclerView.setAdapter(adapter);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("AAA","search error : "+error);
+                    }
+                });
+                requestQueue.add(request);
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        adapter = new RecyclerViewAdapter(context,sportInfoArrayList);
+        recyclerView.setAdapter(adapter);
+        super.onResume();
+    }
+}
