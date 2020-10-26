160157df6da36c9abe0d6b36710ac6136b650b56
sen9040 <62924580+sen9040@users.noreply.github.com>
2020-10-26 15:38:05 +0900

    정리

--- contest-server/routes/search.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getSearch, getAreaSearch } = require("../controllers/search");
-
-const router = express.Router();
-
-router.route("/").get(getSearch);
-router.route("/area").get(getAreaSearch);
-
-module.exports = router;
